585037dbf63107871ed1921679f17431981d6812
Mitch Guth <MitchGuth@MacBook-Pro-4.local>
2018-07-10 09:17:53 -0400

    V 0.0.1

---
+++ pong.py
@@ -0,0 +1,61 @@
+import pygame
+import random
+
+def game():
+    arena_width = 800
+    arena_height = 600
+    pygame.init()
+    screen = pygame.display.set_mode((800,600))
+    done = False
+
+    clock = pygame.time.Clock()
+
+    ball_x = 400
+    ball_y = 300
+    ball_radius = 20
+    ball_mvmnt_x = 0
+    ball_mvmnt_y = 0
+
+    tipoff = random.randint(1, 6)
+    if tipoff == 1:
+        ball_mvmnt_x += 10
+        ball_mvmnt_y += 10
+    elif tipoff == 2:
+        ball_mvmnt_x += ball_mvmnt_x - 10
+        ball_mvmnt_y += ball_mvmnt_y - 10
+    elif tipoff == 3:
+        ball_mvmnt_x += 10
+    elif tipoff == 4: 
+        ball_mvmnt_x += ball_mvmnt_x - 10
+    elif tipoff == 5: 
+        ball_mvmnt_x += 10
+        ball_mvmnt_y += ball_mvmnt_y - 10
+    elif tipoff == 6:
+        ball_mvmnt_x += ball_mvmnt_x - 10
+        ball_mvmnt_y += 10
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        screen.fill((0, 0, 0))
+        pygame.draw.circle(screen, (255, 0, 0), (ball_x, ball_y), ball_radius, 0)
+        pygame.display.update()
+        clock.tick(60)
+
+        ball_x += ball_mvmnt_x
+        ball_y += ball_mvmnt_y
+        if ball_x + ball_radius > arena_width:
+            ball_mvmnt_x = -ball_mvmnt_x
+        if ball_y + ball_radius > arena_height:
+            ball_mvmnt_y = -ball_mvmnt_y
+        if ball_x - ball_radius < 0:
+            ball_mvmnt_x = -ball_mvmnt_x
+        if ball_y - ball_radius < 0:
+            ball_mvmnt_y = -ball_mvmnt_y
+        
+
+pygame.quit()
+
+game()
