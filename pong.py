9c73dcdb367801ac232e23ef63216b176c6014ca
Mitch Guth <MitchGuth@MacBook-Pro-4.local>
2018-07-11 18:39:16 -0400

    V 0.1.0 Player 2 added adn game() removed as it is not needed.

--- pong.py
+++ pong.py
@@ -1,103 +1,144 @@
 import pygame
 import random
 
-def you_lose():
-    print "You have lost the game, good luck next time. "
+WIDTH = 800
+HEIGHT = 600
+#define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
-def game():
-    arena_width = 800
-    arena_height = 600
-    pygame.init()
-    screen = pygame.display.set_mode((800,600))
-    done = False
-    clock = pygame.time.Clock()
+#initialize window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+done = False
+clock = pygame.time.Clock()
+#process input
+#update/render
+#draw
+ball_x = 400
+ball_y = 300
+ball_radius = 20
+ball_mvmnt_x = 0
+ball_mvmnt_y = 0
+paddle_x = 1
+paddle2_x = 794
+paddle_y_top = 260
+paddle_y_bottom = 340
+paddle2_y_top = 260
+paddle2_y_bottom = 340
 
-    ball_x = 400
-    ball_y = 300
-    ball_radius = 20
-    ball_mvmnt_x = 0
-    ball_mvmnt_y = 0
-    paddle_x = 1
-    paddle_y_top = 260
-    paddle_y_bottom = 340
+
+def paddle_1():
+    pygame.draw.line(screen, GREEN, [paddle_x, paddle_y_top], [paddle_x, paddle_y_bottom], 10)
+
+def paddle_2():
+    pygame.draw.line(screen, BLUE, [paddle2_x, paddle2_y_top], [paddle2_x, paddle2_y_bottom], 10)
+
+def score():
+    pass
+
+tipoff = random.randint(1, 6)
+if tipoff == 1:
+    ball_mvmnt_x += 10
+    ball_mvmnt_y += 10
+elif tipoff == 2:
+    ball_mvmnt_x += ball_mvmnt_x - 10
+    ball_mvmnt_y += ball_mvmnt_y - 10
+elif tipoff == 3:
+    ball_mvmnt_x += 10
+elif tipoff == 4: 
+    ball_mvmnt_x += ball_mvmnt_x - 10
+elif tipoff == 5: 
+    ball_mvmnt_x += 10
+    ball_mvmnt_y += ball_mvmnt_y - 10
+elif tipoff == 6:
+    ball_mvmnt_x += ball_mvmnt_x - 10
+    ball_mvmnt_y += 10
+#gameloop
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    screen.fill((0, 0, 0))
+    pygame.draw.circle(screen, RED, (ball_x, ball_y), ball_radius, 0)
+    paddle_1()
+    paddle_2()
+    pygame.display.update()
+    clock.tick(60)
+
+
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_UP]:
+        if paddle2_y_top > 0:
+            paddle2_y_top -= 10
+            paddle2_y_bottom -= 10
+    if pressed[pygame.K_DOWN]:
+        if paddle2_y_bottom < 600:
+            paddle2_y_top += 10
+            paddle2_y_bottom += 10
+    if pressed[pygame.K_q]:
+        if paddle_y_top > 0:
+            paddle_y_top -= 10
+            paddle_y_bottom -= 10
+    if pressed[pygame.K_a]:
+        if paddle_y_bottom < 600:
+            paddle_y_top += 10
+            paddle_y_bottom += 10
     
-    def paddle_1():
-
-        pygame.draw.line(screen, (0,255, 0), [paddle_x, paddle_y_top], [paddle_x, paddle_y_bottom], 10)
-
-    tipoff = random.randint(1, 6)
-    if tipoff == 1:
-        ball_mvmnt_x += 10
-        ball_mvmnt_y += 10
-    elif tipoff == 2:
-        ball_mvmnt_x += ball_mvmnt_x - 10
-        ball_mvmnt_y += ball_mvmnt_y - 10
-    elif tipoff == 3:
-        ball_mvmnt_x += 10
-    elif tipoff == 4: 
-        ball_mvmnt_x += ball_mvmnt_x - 10
-    elif tipoff == 5: 
-        ball_mvmnt_x += 10
-        ball_mvmnt_y += ball_mvmnt_y - 10
-    elif tipoff == 6:
-        ball_mvmnt_x += ball_mvmnt_x - 10
-        ball_mvmnt_y += 10
-
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-
-        screen.fill((0, 0, 0))
-        pygame.draw.circle(screen, (255, 0, 0), (ball_x, ball_y), ball_radius, 0)
-        paddle_1()
-        pygame.display.update()
-        clock.tick(60)
-
-
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]:
-            if paddle_y_top > 0:
-                paddle_y_top -= 10
-                paddle_y_bottom -= 10
-        if pressed[pygame.K_DOWN]:
-            if paddle_y_bottom < 600:
-                paddle_y_top += 10
-                paddle_y_bottom += 10
-        
-        ball_x += ball_mvmnt_x
-        ball_y += ball_mvmnt_y
-        paddle_top = range(paddle_y_top, paddle_y_top + 20)
-        paddle_bottom = range(paddle_y_bottom, paddle_y_bottom - 20)
-
-        if ball_x + ball_radius > arena_width:
-            ball_mvmnt_x = -ball_mvmnt_x
-        if ball_y + ball_radius > arena_height:
-            ball_mvmnt_y = -ball_mvmnt_y
-        if ball_x == paddle_x and ball_y >= paddle_y_top and ball_y <= paddle_y_bottom :
-            ball_mvmnt_x = -ball_mvmnt_x 
-        if ball_y - ball_radius < 0:
-            ball_mvmnt_y = -ball_mvmnt_y
-        if ball_x - ball_radius < 0: 
-            if  ball_y <= paddle_y_bottom and ball_y >= paddle_y_top:
-                if ball_y not in paddle_top and ball_y not in paddle_bottom:
-                    options = random.randint(1, 2)
-                    if options == 1:
-                        ball_mvmnt_y += random.randint(1, 7)
-                        ball_mvmnt_x = -ball_mvmnt_x
-                    else:
-                        ball_mvmnt_y -= random.randint(1, 7)
-                        ball_mvmnt_x = -ball_mvmnt_x
-                else:
+    ball_x += ball_mvmnt_x
+    ball_y += ball_mvmnt_y
+    paddle_top = range(paddle_y_top, paddle_y_top + 20)
+    paddle_bottom = range(paddle_y_bottom, paddle_y_bottom - 20)
+    paddle2_top = range(paddle2_y_top, paddle2_y_top + 20)
+    paddle2_bottom = range(paddle2_y_bottom, paddle2_y_bottom -20)
+    #paddle2 logic
+    if ball_x + ball_radius == WIDTH:
+        if ball_y <= paddle2_y_bottom and ball_y >= paddle2_y_top:
+            if ball_y not in paddle2_top and ball_y not in paddle2_bottom:
+                options = random.randint(1, 2)
+                if options == 1:
+                    ball_mvmnt_y += random.randint(1, 7)
                     ball_mvmnt_x = -ball_mvmnt_x
-            #define middle and then adjust y mvmnt based on middle of paddle
+                else:
+                    ball_mvmnt_y -= random.randint(1, 7)
+                    ball_mvmnt_x = -ball_mvmnt_x
+            else: 
+                ball_mvmnt_x = -ball_mvmnt_x
+        ball_mvmnt_x = -ball_mvmnt_x
+    #floor logic
+    if ball_y + ball_radius > HEIGHT:
+        ball_mvmnt_y = -ball_mvmnt_y
+    #ceiling logic
+    if ball_y - ball_radius < 0:
+        ball_mvmnt_y = -ball_mvmnt_y
+    
+    # if ball_x == paddle_x and ball_y >= paddle_y_top and ball_y <= paddle_y_bottom :
+    #     ball_mvmnt_x = -ball_mvmnt_x 
+    
+    if ball_x - ball_radius == 0: 
+        if  ball_y <= paddle_y_bottom and ball_y >= paddle_y_top:
+            if ball_y not in paddle_top and ball_y not in paddle_bottom:
+                options = random.randint(1, 2)
+                if options == 1:
+                    ball_mvmnt_y += random.randint(1, 7)
+                    ball_mvmnt_x = -ball_mvmnt_x
+                else:
+                    ball_mvmnt_y -= random.randint(1, 7)
+                    ball_mvmnt_x = -ball_mvmnt_x
             else:
-                you_lose()
-                break
+                ball_mvmnt_x = -ball_mvmnt_x
+        
+        else:
+            score()
+                
         
 pygame.quit()
 
-game()
 
 # need to create if/else logic to apply the paddle top and bottom 
 # so that I can change the trejectory of a ball that is straight east west moving.
